a4466b037418813c21f1b171fe472ac44b0e0e4a
joshcasteel <joshcasteelz@gmail.com>
2019-03-18 16:06:52 -0400

    views and routes updated

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Task;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+    }
 }
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,8 @@
+@extends('tasks.layout')
+
+@section('content')
+<h1>Tasks</h1>
+
+
+
+@endsection
